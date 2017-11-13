6865be53e4dc6f238f36cf4a02c6916cf486ffd7
Simon Skov <skov.simon3@gmail.com>
2017-11-13 07:58:23 +0100

    nyt decal system

---
+++ Assets/Plugins/Decals/Editor/DecalWindow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DecalWindow : EditorWindow
+{
+
+    static DecalWindow win;
+
+    string btnCreate = "Add Decal To Scene";
+    
+    Sprite decal;
+
+
+    [MenuItem("Window/Decals")]
+    public static void ShowWindow() {
+        win = GetWindow<DecalWindow>();
+        win.Show();
+    }
+    
+    void OnGUI()
+    {
+
+        float marginLeft = this.position.width / 2 - 100 / 2;
+        decal = (Sprite)EditorGUI.ObjectField(new Rect(marginLeft, 5, 100, 100), decal, typeof(Sprite), true);
+
+        GUILayout.BeginArea(new Rect(5, 125, this.position.width - 10, 32));
+        if (GUILayout.Button(btnCreate))
+            Button_CreateDecal_Click();
+        GUILayout.EndArea();
+                
+    }
+    
+
+    private void Button_CreateDecal_Click()
+    {        
+        GameObject go = new GameObject("Decal");
+        go.AddComponent<MeshFilter>();
+        go.AddComponent<MeshRenderer>();
+        var decalObj = go.GetComponent<DecalObject>() ?? go.AddComponent<DecalObject>();
+        
+        decalObj.image = decal;
+        decalObj.BuildDecal();
+
+        Selection.activeGameObject = go;
+    }
+}
---
+++ Assets/Plugins/Decals/Scripts/DecalObject.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class DecalObject : MonoBehaviour {
+
+    private MeshFilter filter;
+    private new MeshRenderer renderer;
+
+    public Sprite image;
+    public float offset;
+
+    public void BuildDecal()
+    {
+
+        filter = gameObject.GetComponent<MeshFilter>() != null ? gameObject.GetComponent<MeshFilter>() : gameObject.AddComponent<MeshFilter>();
+        renderer = gameObject.GetComponent<MeshRenderer>() != null ? gameObject.GetComponent<MeshRenderer>() : gameObject.AddComponent<MeshRenderer>();
+
+        filter.mesh = CreateMesh(transform.localScale.x, transform.localScale.y);
+
+        var tempMaterial = new Material(Shader.Find("Transparent/Diffuse"));
+        var croppedTexture = new Texture2D((int)image.rect.width, (int)image.rect.height);
+        var pixels = image.texture.GetPixels((int)image.textureRect.x,
+                                            (int)image.textureRect.y,
+                                            (int)image.textureRect.width,
+                                            (int)image.textureRect.height);
+
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+
+        //tempMaterial.shader = Shader.Find("Transparent/Diffuse");
+        tempMaterial.mainTexture = croppedTexture;
+        renderer.sharedMaterial = tempMaterial;
+    }
+
+    Mesh CreateMesh(float width, float height)
+    {
+        width = width / 2;
+        height = height / 2;
+
+        Mesh m = new Mesh();
+        m.name = "Decal";
+        m.vertices = new Vector3[] {
+         new Vector3(width, height, offset),            //Top right
+         new Vector3(-width, height,offset),        //Bottom right
+         new Vector3(-width, -height, offset),     //bottom left
+         new Vector3(width, -height, offset)     //top left 
+     };
+        m.uv = new Vector2[] {
+         new Vector2 (0, 1),
+         new Vector2 (1, 1),
+         new Vector2(1, 0),
+         new Vector2 (0, 0)
+     };
+        m.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        m.RecalculateNormals();
+
+        return m;
+    }
+}
