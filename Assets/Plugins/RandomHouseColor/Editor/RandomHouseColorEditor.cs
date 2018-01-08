eef6a8ed6d392fde0108a05f68fca83c45c281be
ZepplinZy <kim.danborg@hotmail.com>
2018-01-08 14:58:29 +0100

    Færdig

--- Assets/Plugins/RandomHouseColor/Editor/RandomHouseColorEditor.cs
+++ Assets/Plugins/RandomHouseColor/Editor/RandomHouseColorEditor.cs
@@ -8,6 +8,18 @@ public class RandomHouseColorEditor : Editor {
 
     public override void OnInspectorGUI()
     {
+        var controller = target as RandomHouseColorParent;
+        EditorGUIUtility.LookLikeInspector();
+        SerializedProperty tps = serializedObject.FindProperty("m_colors");
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(tps, true);
+        if (EditorGUI.EndChangeCheck())
+            serializedObject.ApplyModifiedProperties();
+        EditorGUIUtility.LookLikeControls();
+
+
+        //((RandomHouseColorParent)target).m_colors = (Material[])EditorGUILayout.ObjectField("", ((RandomHouseColorParent)target).m_colors, typeof(Material[]), false);
+
         if (GUILayout.Button("I am a button"))
         {
 
--- Assets/Plugins/RandomHouseColor/Script/RandomHouseColorParent.cs
+++ Assets/Plugins/RandomHouseColor/Script/RandomHouseColorParent.cs
@@ -12,6 +12,11 @@ public class RandomHouseColorParent : MonoBehaviour {
 
     public void NoName()
     {
+        if (m_colors.Length == 0)
+        {
+            return;
+        }
+
         GameObject[] houses = new GameObject[transform.childCount];
 
         for (int i = 0; i < houses.Length; i++)
@@ -22,8 +27,13 @@ public class RandomHouseColorParent : MonoBehaviour {
 
         foreach (var house in houses)
         {
-            Material color = m_colors[Random.Range(0, m_colors.Length)];
             
+            Material replace = m_colors[Random.Range(0, m_colors.Length)];
+
+            foreach (var find in m_colors)
+            {
+                ReplaceMaterial(house, find, replace);
+            }
 
             //Debug.Log(house.transform.position);
             //var hits = Physics.RaycastAll(house.transform.position, -house.transform.right, 2);
@@ -41,6 +51,43 @@ public class RandomHouseColorParent : MonoBehaviour {
         }
 
     }
-    
+
+    public static void ReplaceMaterial(GameObject go, Material find, Material replace, bool includeChild = true)
+    {
+
+        SetReplaceMaterial(go, find, replace);
+        if (includeChild)
+        {
+            for (int i = 0; i < go.transform.childCount; i++)
+            {
+                GameObject child = go.transform.GetChild(i).gameObject;
+                SetReplaceMaterial(child, find, replace);
+            }
+        }
+
+    }
+
+    private static Material GetMaterial(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<Renderer>() != null)
+        {
+            return gameObject.GetComponent<Renderer>().sharedMaterial;
+        }
+        return null;
+    }
+
+    private static bool HasMaterial(GameObject go, Material material)
+    {
+        return GetMaterial(go) == material;
+    }
+
+    private static void SetReplaceMaterial(GameObject go, Material find, Material replace)
+    {
+        if (HasMaterial(go, find))
+        {
+            go.GetComponent<Renderer>().sharedMaterial = replace;
+        }
+    }
+
 
 }
