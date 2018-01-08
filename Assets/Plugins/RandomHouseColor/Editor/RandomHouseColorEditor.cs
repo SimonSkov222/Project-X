9e77f26ff9474e0121e4649e597bbe768a2785b7
ZepplinZy <kim.danborg@hotmail.com>
2018-01-08 13:39:55 +0100

    påbegyndt random hus farver

---
+++ Assets/Plugins/RandomHouseColor/Editor/RandomHouseColorEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RandomHouseColorParent))]
+public class RandomHouseColorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("I am a button"))
+        {
+
+            Debug.Log("hej");
+            ((RandomHouseColorParent)target).NoName();
+
+        }
+        
+    }
+
+
+
+
+}
---
+++ Assets/Plugins/RandomHouseColor/Script/RandomHouseColorParent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomHouseColorParent : MonoBehaviour {
+
+    public Material[] m_colors;
+
+
+    
+
+    public void NoName()
+    {
+        GameObject[] houses = new GameObject[transform.childCount];
+
+        for (int i = 0; i < houses.Length; i++)
+        {
+
+            houses[i] = transform.GetChild(i).gameObject;
+        }
+
+        foreach (var house in houses)
+        {
+            Material color = m_colors[Random.Range(0, m_colors.Length)];
+            
+
+            //Debug.Log(house.transform.position);
+            //var hits = Physics.RaycastAll(house.transform.position, -house.transform.right, 2);
+            //if (hits.Length > 0)
+            //{
+            //    Debug.Log("true");
+
+            //    foreach (var item in hits)
+            //    {
+            //        Debug.Log(item.collider.transform.position);
+            //    }
+            //    break;
+            //}
+            
+        }
+
+    }
+    
+
+}
