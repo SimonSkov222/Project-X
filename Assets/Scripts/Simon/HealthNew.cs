03052309dd0c06ca7bd3c001c5e33bcab1fd1ec6
skov.simon3 <skov.simon3@gmail.com>
2018-02-14 10:11:43 +0100

    meta filer

---
+++ Assets/Scripts/Simon/HealthNew.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface HealthNew {
+    int HealthMax { get; set; }
+    int Health { get; set; }
+
+    void OnDeath();
+
+}
