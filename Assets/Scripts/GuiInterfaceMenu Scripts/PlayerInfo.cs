ef1d41529af81b8e3948a40e6b65f1d3a9909bf8
Kim Danborg <kim.danborg@hotmail.com>
2018-03-05 23:59:18 +0100

    Næsten færdig med GuiInterfaceMenu Mangler bare noget feedback og nogle scripts primært hent data

---
+++ Assets/Scripts/GuiInterfaceMenu Scripts/PlayerInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI CharacterName;
+    public TextMeshProUGUI Level;
+
+    int LevelI = 2;
+
+    public Image PictureIcon;
+    public Image PictureCharacter;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Name.text = "!!!!!!!!Name!!!!!!!";
+        CharacterName.text = "!!!!!!!!CharacterName!!!!!!!!!!!";
+        Level.text = LevelI.ToString();
+        //PictureIcon = PictureIcon
+        //PictureCharacter = PictureCharacter
+
+
+	}
+}
