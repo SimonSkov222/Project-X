9a3a79b0f944a71fa23972f96eca086e4b984e70
Kim Danborg <kim.danborg@hotmail.com>
2018-03-07 16:58:33 +0100

    import textmeshpro

---
+++ Assets/Scripts/GuiPlayer/GuiPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GuiPlayer : MonoBehaviour {
+
+    public TextMeshProUGUI textAmmo;
+
+    public GameObject player;
+
+    public Weapon weapon;
+
+
+    // Use this for initialization
+    void Start () {
+
+        weapon = player.GetComponent<Weapon>();
+
+        Debug.Log(weapon.CurrentAmmo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //textAmmo.text = weapon.CurrentAmmo.ToString();
+	}
+}
